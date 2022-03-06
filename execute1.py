201ce59ce379b13e29429ee6aacdcc037a9f2df1
Naresh M <91264962+naresh-webdev@users.noreply.github.com>
2022-03-06 20:30:07 +0530

    Update execute1.py

--- execute1.py
+++ execute1.py
@@ -12,7 +12,7 @@ cur.execute('use athish_new')
 
 #  -- this is for creating the product table which is basically the market--
 cur.execute(
-    'create table product(id int,name varchar(30),qty int,price_per_item float)')
+    'create table product(id int primary key,name varchar(30),qty int,price_per_item float)')
 cur.execute("insert into product values(1,'apples',30,40),(2,'oranges',40,20),(3,'mango',30,25),(4,'strawberry',80,20),(5,'watermelon',20,60)")
 x.commit()
 
